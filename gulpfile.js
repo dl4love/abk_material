e265763e0ae8e6bd8c86f40e73093ca334a2b8d2
me <noreply@domain.com>
2019-12-01 21:26:37 +0100

    file watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, parallel } = require('gulp');
+const { src, dest, parallel, watch } = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const plugins = gulpLoadPlugins();
 
@@ -6,10 +6,14 @@ function material_blue() {
   const theme_name = 'material_blue';
 
   return src(`./src/${theme_name}/styles.scss`)
-      .pipe(plugins.sass())
-      .pipe(plugins.csscomb())
-      .pipe(plugins.cssbeautify({indent: '  '}))
-      .pipe(dest(`./dest/${theme_name}/`));
+    .pipe(plugins.sass())
+    .pipe(plugins.csscomb())
+    .pipe(plugins.cssbeautify({indent: '  '}))
+    .pipe(dest(`./dest/${theme_name}/`));
 }
 
 exports.prod = parallel(material_blue);
+exports.watch = function() {
+  watch('./src/material_blue/*.scss', material_blue);
+  watch('./src/material_blue/components/*.scss', material_blue);
+};
